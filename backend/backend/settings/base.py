fff86fbc3f53aa37373be3660336c451a54d5915
kodyka <131517845+kodyka@users.noreply.github.com>
2023-05-18 13:09:19 +0000

    init lang and timezone

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -129,9 +129,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "pl-pl"
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "Europe/Warsaw"
+TIME_ZONE = "Asia/Yekaterinburg"
 
 USE_I18N = True
 
